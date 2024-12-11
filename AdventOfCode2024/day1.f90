d1e899dd06bd5743dcfc36ff1675524c51ef3fcc
MindokRuvaak <cole_daniel@rocketmail.com>
2024-12-11 11:38:21 +0100

    day1 task2 solution also pushed

--- AdventOfCode2024/day1.f90
+++ AdventOfCode2024/day1.f90
@@ -1,8 +1,8 @@
 program LoadDiffSum
    use sorting
-   integer :: io, i, f
+   integer :: io, i, f, similarityMult, j, similarrityScoreAccumulator
    integer, dimension(2000) :: a
-   integer, dimension(1000) :: b, c, e
+   integer, dimension(1000) :: b, c, e!, g
    open(newunit=io, file="day1.dat", status="old", action="read")
    read(io, *) a
 
@@ -12,7 +12,6 @@ program LoadDiffSum
       c(i) = a(2*i)
    end do
 
-   !comments
 
    ! sort the two vectors
    call quicksort(b,1,1000)
@@ -23,6 +22,22 @@ program LoadDiffSum
    end do
    f = sum(e)
    ! print *, b
-   write(*,*) (b(i), c(i), e(i), new_line("a"), i = 1, 1000)
+   ! write(*,*) (b(i), c(i), e(i), new_line("a"), i = 1, 1000)
+   print*, "Task1:"
    print*, f
+
+
+   outer : do i = 1,1000
+      similarityMult  = 0
+      inner : do j = 1,1000
+         if (b(i) == c(j)) then
+            similarityMult = similarityMult + 1
+         end if
+      end do inner
+      similarrityScoreAccumulator = similarrityScoreAccumulator + b(i)*similarityMult
+   end do outer
+
+   print*, "Task2:"
+   print*, similarrityScoreAccumulator
+
 end program LoadDiffSum
